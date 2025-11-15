3335a7ebd42692af7d6bd0683578b6e4ba2485b5
Blutjaeger11 <banerjeesouvik1998@gmail.com>
2025-11-15 15:23:20 +0530

    variables intro

---
+++ 01-python-foundations/scripts/01_variables.py
@@ -0,0 +1,11 @@
+# Basic variable types in Python
+
+name = "Souvik"
+age = 25
+is_cybersecurity_professional = True
+skills = ["Networking", "Linux", "Threat Hunting", "Python"]
+
+print("Name:", name)
+print("Age:", age)
+print("Cybersecurity Professional:", is_cybersecurity_professional)
+print("Skills:", skills)
